0a1635d9d6693e5c317969dcfd3e797da64c96f3
dakong <dakong94@gmail.com>
2016-02-24 00:04:57 -0800

    Update Patient Queue works from Check In

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,31 +12,47 @@ app.all('/', function(request, response) {
 	response.status(404).send('Page not found.');
 });
 
-var visitor ={
-		name: "Eric",
-		appointment: "2:30PM",
-		checkin: "2:15PM"
-	}
 
-	var visitorTwo ={
-		name: "Daniel",
-		appointment: "2:15PM",
-		checkin: "2:03PM"
-	}
+var visitors = [];
+app.post('/process_post', urlencodedParser, function (req, res) {
+	console.log("post receieved");
+	// Prepare output in JSON format
+	response = {
+		firstName:req.body.first,
+		lastName:req.body.last,
+		appointment:req.body.appointment_time
+	};
 
-	var visitorThree ={
-		name: "Bob",
-		appointment: "2:10PM",
-		checkin: "2:05PM"
-	}
+	response.checkin = getCurrentTime();
+	console.log(response);
 
-	var visitorFour ={
-		name: "Joe",
-		appointment: "1:40PM",
-		checkin: "1:37PM"
-	}
+	visitors.push(response);
+	io.emit('send list', visitors);
+});
 
-var visitors = [visitor,visitorTwo];
+//Function to get Current Time
+function getCurrentTime(){
+	var currentTime;
+	var today = new Date();
+	var currentHour = today.getHours();
+	var currentMinute = today.getMinutes();
+
+	if(currentHour >= 13){
+		currentHour = currentHour - 12;
+		currentTime = currentHour + ':' + currentMinute + 'PM';
+	}
+	else if(currentHour === 12){
+		currentTime = currentHour + ':' + currentMinute + 'PM';
+	}
+	else if (currentHour === 0)
+		currentTime = 1 + ':' + currentMinute + 'AM';
+	else
+		currentTime = currentHour + ':' + currentMinute + 'AM';
+
+	return currentTime;
+
+}
+
 io.on('connection', function(socket){
   console.log('a user connected');
   io.emit('send list', visitors);
@@ -47,12 +65,6 @@ io.on('updated list',function(updatedList){
 
 server.listen(3000);
 
-var visitor = {
-	name: "Eric",
-	appointment: "2:30PM",
-	checkin: "2:15PM"
-}
-console.log("sending over: " + visitor.name);
 
 
 
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -10,14 +10,32 @@ $(document).ready(function(){
     	refreshList(data);
   	});
 
-  	socket.emit('update list', function(data){
-  		console.log('updating list to database');
-  	});
+    $('.check-in').on('submit', function() {
+        console.log("data submitted");
+        var data = grabFormElements();
+        socket.emit('update list',data);
 
-  	function refreshList(list){
+    });
+
+    function refreshList(list){
   		var compiledHtml = template(list);
     	$('#visitor-list').html(compiledHtml);
   	}
+
+
+
+    //Grabs elements from the checkin and puts it into an object
+    function grabFormElements(){
+        var newVisitor = {};
+        newVisitor.firstName= $('#visitor-first').val();
+        newVisitor.lastName = $('#visitor-last').val();
+        newVisitor.appointment = $('#visitor-appointment').val();
+        newVisitor.checkin = getCurrentTime();
+        return newVisitor;
+    }
+
+    // Function to get the time in when the patient checked in
+
 
 	var source = $("#visitor-list-template").html();
 	var template = Handlebars.compile(source);
